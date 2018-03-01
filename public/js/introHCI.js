a42e42952ae0d79a4c53bb5fb497f99c5543a5b7
Franklin Li <frl003@ucsd.edu>
2018-03-01 10:49:36 -0800

    thirdpartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -35,6 +35,18 @@ function initGestures() {
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+  	e.preventDefault();
+  	console.log("submitting form...")
+  	var rsvpEmail = $("#rsvpEmail").val();
+  	//send post request
+  	$.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(res){
+  	alert("RSVP form successfully submitted!");
+  	$("#rsvpEmail").val(''); //clear form
+  }
 }
 
     $(function(){
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,16 @@ var data = {
 /*
  * GET tasks page.
  */
+
+exports.addRSVP = function(req, res){
+	var rsvpEmail = req.body.rsvpEmail;
+	// prints in terminal when a POST is made
+	console.log(rsvpEmail);
+	data.rsvp.push(rsvpEmail); //add to current data
+	//send task name back so browser knows it worked out
+	res.send(rsvpEmail);
+}
+
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
